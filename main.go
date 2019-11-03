48b529f4d0d2b91db8989e05405bfc5ab07a0cec
Bari <abbassibari@gmail.com>
2019-11-03 16:55:40 +0900

    Add cities names and print titles

--- main.go
+++ main.go
@@ -16,7 +16,14 @@ func main() {
 	//Read N the number of aliens
 	N, err := strconv.Atoi(os.Args[1]) //the number of aliens
 	if err != nil {
-		fmt.Println("Argument N is not a number")
+		fmt.Println("Argument N should be a number")
+		os.Exit(1)
+	}
+	if N < 1 {
+		fmt.Println("The number of aliens should be greater than 0")
+		os.Exit(1)
+	} else if N > 100 {
+		fmt.Println("Try a smaller number of aliens")
 		os.Exit(1)
 	}
 
@@ -27,6 +34,10 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	fmt.Println("********************")
+	fmt.Println("** Peaceful world **")
+	fmt.Println("********************")
 
 	//Print the file
 	PrintFile(fileLines)
@@ -44,6 +55,10 @@ func main() {
 	//Add N aliens to a random city
 	w.AddAliens(N)
 
+	fmt.Println("*************************************")
+	fmt.Println("** OH NO! ALIENS ARE EVERYWHERE!!! **")
+	fmt.Println("*************************************")
+
 	//If more than 1 alien is added to a city they fight
 	w.Fight()
 
@@ -55,6 +70,10 @@ func main() {
 		counter++
 	}
 	fmt.Println()
+
+	fmt.Println("*******************************")
+	fmt.Println("** Post-alien-invasion world **")
+	fmt.Println("*******************************")
 
 	//Print what the file would look like now
 	newFileLines := w.CreateFileLines()
